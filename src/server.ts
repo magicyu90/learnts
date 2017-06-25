aa73c255fca3cc248b222365c146359b2a8817d0
magicyu90 <magicyu90@gmail.com>
2017-06-25 23:52:39 +0800

    update

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import * as http from 'http';
+
+const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World Hugo\n");
+})
+
+const hostname: string = "127.0.0.1";
+const port: number = 3000;
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
