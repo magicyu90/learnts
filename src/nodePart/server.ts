4a8d4924bef0824b2f497ed90f5aade8e1e706ca
magicyu90 <magicyu90@gmail.com>
2017-06-26 19:09:46 +0800

    change pro arch

--- src/nodePart/server.ts
+++ src/nodePart/server.ts
@@ -3,7 +3,7 @@ import * as http from 'http';
 const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World Hugo\n");
+    res.end("Hello World Hugo \n");
 })
 
 const hostname: string = "127.0.0.1";
